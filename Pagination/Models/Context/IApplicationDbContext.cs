ef7e0261038f6801e68628abd00d49d827e1ed19
CodeSmith <emiolaabdulmueez@gmail.com>
2020-06-16 00:14:29 +0300

    Add project files.

---
+++ Pagination/Models/Context/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Pagination.Models.Context
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Customer> Customers { get; set; }
+
+        Task<int> SaveChanges();
+    }
+}
---
+++ Pagination/Models/Services/IUriService.cs
@@ -0,0 +1,10 @@
+﻿using Pagination.Models.Filter;
+using System;
+
+namespace Pagination.Models.Services
+{
+    public interface IUriService
+    {
+        Uri GetPageUri(PaginationFilter filter, string route);
+    }
+}
